94b9618ceb91946d57eb414829c9d1f4f8953d09
Peter Ho <peterho1024@gmail.com>
2017-05-16 16:42:03 +0800

    init & 1.1.1

---
+++ algorithm/chapter01.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace algorithm
+{
+    [TestFixture]
+    public class chapter01
+    {
+        [Test]
+        public void _01() {
+            Assert.AreEqual(7, (0 + 15) / 2);
+        }
+    }
+}
