2ffb3f5de63fb8581bead085741e09b204895fa3
Peter Ho <peterho1024@gmail.com>
2017-05-16 22:04:49 +0800

    1.1.2

--- algorithm/chapter01.cs
+++ algorithm/chapter01.cs
@@ -8,17 +8,31 @@ namespace algorithm
     {
         [Test]
         public void _01() {
+            // a
             Assert.AreEqual(7, (0 + 15) / 2);
+            // b
+            Assert.Less(Math.Abs(200 - (2.0e-6 * 100000000.1)), 0.0001f);
+            // c
+            Assert.AreEqual(true, true && false || true && true);
         }
 
         [Test]
         public void _02() {
-            Assert.Less(Math.Abs(200 - (2.0e-6 * 100000000.1)), 0.0001f);
+            Assert.AreEqual(typeof(double), ((1 + 2.236) / 2).GetType());
+            Assert.AreEqual(1.618, (1 + 2.236) / 2);
+
+            Assert.AreEqual(typeof(double), (1 + 2 + 3 + 4.0).GetType());
+            Assert.AreEqual(10f, 1 + 2 + 3 + 4.0);
+
+            Assert.AreEqual(typeof(bool), (4.1 >= 4).GetType());
+            Assert.AreEqual(true, 4.1 >= 4);
+
+            Assert.AreEqual(typeof(string), (1 + 2 + "3").GetType());
+            Assert.AreEqual("33", 1 + 2 + "3");
         }
 
         [Test]
         public void _03() {
-            Assert.AreEqual(true, true && false || true && true);
         }
     }
 }
