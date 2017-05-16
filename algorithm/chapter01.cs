35274271b5b02b494c28b1de090f096ee2e31abf
Peter Ho <peterho1024@gmail.com>
2017-05-16 16:48:13 +0800

    1.1.1

--- algorithm/chapter01.cs
+++ algorithm/chapter01.cs
@@ -4,11 +4,21 @@ using NUnit.Framework;
 namespace algorithm
 {
     [TestFixture]
-    public class chapter01
+    public class chapter_1_1
     {
         [Test]
         public void _01() {
             Assert.AreEqual(7, (0 + 15) / 2);
+        }
+
+        [Test]
+        public void _02() {
+            Assert.Less(Math.Abs(200 - (2.0e-6 * 100000000.1)), 0.0001f);
+        }
+
+        [Test]
+        public void _03() {
+            Assert.AreEqual(true, true && false || true && true);
         }
     }
 }
